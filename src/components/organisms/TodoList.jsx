eba25d72a9a7d4e7d2562cf721b0d916c52f2810
Akash09082001 <aakashl@letsupgrade.in>
2024-08-20 16:50:04 +0530

    add: loading added to dashboard

--- src/components/organisms/TodoList.jsx
+++ src/components/organisms/TodoList.jsx
@@ -1,17 +1,20 @@
-"use client";
+"use client"
 
-import React, { useEffect, useState } from 'react';
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '../ui/card';
-import DeleteTodo from '../atoms/DeleteTodo';
-import EditTodo from './EditTodo';
 import { getTodos } from '@/actions/services';
+import { useEffect, useState } from 'react';
+import DeleteTodo from '../atoms/DeleteTodo';
+import SkeletonCard from '../molecules/SkeletonCard';
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '../ui/card';
+import EditTodo from './EditTodo';
 import EmptyState from './EmptyState';
 
 const TodoList = () => {
     const [todos, setTodos] = useState([]);
     const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
+        setLoading(true)
         const fetchTodos = async () => {
             try {
                 const res = await getTodos();
@@ -19,43 +22,47 @@ const TodoList = () => {
                     throw new Error("Failed to fetch todo list");
                 }
                 setTodos(res.todos);
+                setLoading(false)
             } catch (error) {
                 setError(error.message);
+                setLoading(false)
                 console.error("Failed to Fetch Todos:", error);
             }
         };
         fetchTodos();
     }, []);
 
-    if (error) {
-        return <div>Error: {error}</div>;
-    }
-
-    if (todos.length === 0) {
-        return (
-            <div className="flex w-full flex-grow flex-1 items-center justify-center">
-                <EmptyState />
-            </div>
-        )
-    }
-
     return (
-        <div className='grid grid-cols-1 md:grid-cols-2 gap-5 h-fit lg:grid-cols-3 w-full'>
-            {todos.map(todo => (
-                <Card key={todo._id} className="backdrop-blur-sm h-fit w-full">
-                    <CardHeader className="flex flex-row gap-2 items-center w-full">
-                        <CardTitle className="w-full text-base lg:text-lg">{todo.title}</CardTitle>
-                        <EditTodo id={todo._id} />
-                        <DeleteTodo id={todo._id} />
-                    </CardHeader>
-                    <CardContent>
-                        <CardDescription>
-                            {todo.description}
-                        </CardDescription>
-                    </CardContent>
-                </Card>
-            ))}
-        </div>
+        <>
+            {
+                loading ? (
+                    <SkeletonCard />
+                ) : (
+                    todos.length === 0 ? (
+                        <div className="flex w-full flex-grow flex-1 items-center justify-center">
+                            <EmptyState />
+                        </div>
+                    ) : (
+                        <div className='grid grid-cols-1 md:grid-cols-2 gap-5 h-fit lg:grid-cols-3 w-full'>
+                            {todos.map(todo => (
+                                <Card key={todo._id} className="backdrop-blur-sm h-full w-full">
+                                    <CardHeader className="flex flex-row gap-2 items-center w-full">
+                                        <CardTitle className="w-full text-base lg:text-lg">{todo.title}</CardTitle>
+                                        <EditTodo id={todo._id} />
+                                        <DeleteTodo id={todo._id} />
+                                    </CardHeader>
+                                    <CardContent>
+                                        <CardDescription className="s scrollBar-hide">
+                                            {todo.description}
+                                        </CardDescription>
+                                    </CardContent>
+                                </Card>
+                            ))}
+                        </div>
+                    )
+                )
+            }
+        </>
     );
 };
 
